7915196fdd743e07753bbca6da9ca6544344f740
taechanha <hataeck@gmail.com>
2021-11-01 23:53:18 +0900

    add comment

--- isPalindrome.py
+++ isPalindrome.py
@@ -22,14 +22,15 @@ def check(elems):
 
 
 # Runner method
-rev = None
-slow = fast = head
-while fast and fast.next:
-    fast = fast.next.next
-    rev, rev.next, slow = slow, rev, slow.next
-if fast:
-    slow = slow.next
 
-while rev and rev.val == slow.val:
-    slow, rev = slow.next, rev.next
-return not rev
+# rev = None
+# slow = fast = head
+# while fast and fast.next:
+#     fast = fast.next.next
+#     rev, rev.next, slow = slow, rev, slow.next
+# if fast:
+#     slow = slow.next
+
+# while rev and rev.val == slow.val:
+#     slow, rev = slow.next, rev.next
+# return not rev
