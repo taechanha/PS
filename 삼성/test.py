fcc2d20af04c5b075014e92382f3c54493e5e864
tcha <hataeck@gmail.com>
2022-10-21 18:17:39 +0900

    rm

--- 삼성/test.py
+++
@@ -1,3 +0,0 @@
-board = [list(map(int, input().split())) for _ in range(11)]
-
-print(sum([row.count(0) for row in board]))
--- 삼성/조합.py
+++
@@ -1,5 +0,0 @@
-from math import comb
-
-n, m = map(int, input().split())
-res = comb(n, m)
-print(res)
